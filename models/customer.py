7f797a5cc6432848f3777048d3738c8fbc2d25cf
Chesney Hardin <chesney.hardin@gmail.com>
2023-07-17 15:12:53 -0500

    get requests based on query string parameters working

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,7 @@
 class Customer():
     """this class will serve as a blueprint for all objects that get created for a customer.
     """
-    def __init__(self, id, name, address, email, password):
+    def __init__(self, id, name, address, email = "", password = ""):
         self.id = id
         self.name = name
         self.address = address
